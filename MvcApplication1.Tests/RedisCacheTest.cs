a132ed76c2a8a3e89bbd880d2de764a13c3a1ab2
hieubv <hieubv.cntt@gmail.com>
2017-07-15 15:37:35 +0700

    use package StackExchange.Redis

---
+++ MvcApplication1.Tests/RedisCacheTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StackExchange.Redis;
+
+namespace MvcApplication1.Tests
+{
+    [TestClass]
+    public class RedisCacheTest
+    {
+        [TestMethod]
+        public void RedisConnection_BasicUsage()
+        {
+            // Chuỗi kết nối đến Redis
+            var redis = ConnectionMultiplexer.Connect("127.0.0.1:6379");
+            //Kết nối đến db của Redis
+            IDatabase db = redis.GetDatabase();
+        }
+    }
+}
