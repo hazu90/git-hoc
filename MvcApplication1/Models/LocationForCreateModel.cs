b855b79763fe2d5c65eccc03ccdb127b06f0946f
hazu90 <hieubv.cntt@gmail.com>
2017-07-18 06:28:49 +0700

    commit 18/07/2017

---
+++ MvcApplication1/Models/LocationForCreateModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1.Models
+{
+    public class LocationForCreateModel
+    {
+        public string LocationName { get; set; }
+        public string Address { get; set; }
+
+    }
+}
