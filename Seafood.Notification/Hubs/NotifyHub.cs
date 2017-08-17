1e42376db4df929e33447a0e4ecf27fd53b0c46b
hieubv <hieubv.cntt@gmail.com>
2017-08-17 08:42:15 +0700

    commit code for notify hub

---
+++ Seafood.Notification/Hubs/NotifyHub.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Seafood.Notification.Hubs
+{
+    public class NotifyHub:Hub
+    {
+        public override System.Threading.Tasks.Task OnConnected()
+        {
+            return base.OnConnected();
+        }
+        public override System.Threading.Tasks.Task OnDisconnected(bool stopCalled)
+        {
+            return base.OnDisconnected(stopCalled);
+        }
+
+
+    }
+}
---
+++ Seafood.Notification/Models/NotificationModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Seafood.Notification.Models
+{
+    public class NotificationModel
+    {
+        public string Sender { get; set; }
+        public string Receiver { get; set; }
+        public int Type { get; set; }
+        public string Content { get; set; }
+    }
+}
