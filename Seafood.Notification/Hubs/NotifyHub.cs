06e4c9a90cc23f61616207796ffa965de3cd953a
hieubv <hieubv.cntt@gmail.com>
2017-08-18 17:59:32 +0700

    comit code notify

--- Seafood.Notification/Hubs/NotifyHub.cs
+++ Seafood.Notification/Hubs/NotifyHub.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNet.SignalR;
+using Seafood.Notification.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,6 +18,9 @@ namespace Seafood.Notification.Hubs
             return base.OnDisconnected(stopCalled);
         }
 
-
+        public List<NotificationModel> Send()
+        {
+            return new List<NotificationModel>();
+        }
     }
 }
