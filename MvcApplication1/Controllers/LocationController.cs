5e78878889d37be3f28187c81ff46f265019ab97
hazu90 <hieubv.cntt@gmail.com>
2017-12-08 06:11:07 +0700

    commit from home 08/12/2017

--- MvcApplication1/Controllers/LocationController.cs
+++ MvcApplication1/Controllers/LocationController.cs
@@ -48,9 +48,30 @@ namespace MvcApplication1.Controllers
         }
         
         [AllowAnonymous]
-        public ActionResult Detail(string link)
+        public ActionResult Detail(string location, string link)
         {
-            return View();
+            var locationModel = new LocationForDetailModel();
+            locationModel.RepImgLink = @"https://media.foody.vn/res/g32/311541/prof/s576x330/foody-mobile-2-jpg-167-636174156711051802.jpg";
+            locationModel.LocationName = @"Trà Sữa TocoToco - Nguyễn Khang";
+            locationModel.CityName = @"Hà Nội";
+            locationModel.DistrictName = @"Quận Cầu Giấy";
+            locationModel.WardName = @"28B Nguyễn Khang";
+            locationModel.HrefLink = @"/ha-noi/tra-sua-tocotoco-nguyen-khang";
+            locationModel.CityLink = @"/ha-noi/dia-diem";
+            locationModel.DistrictLink = @"/ha-noi/khu-vuc-quan-cau-giay";
+            locationModel.WardLink = @"/ha-noi/khu-vuc-bo-song";
+            locationModel.ViewCount = 422;
+            locationModel.CategoryName = "Café/Dessert";
+            locationModel.CategoryLink = "/ha-noi/food/cafe";
+            locationModel.CulinaryName = "Đài Loan";
+            locationModel.CulinaryLink = "/ha-noi/cafe-phong-cach-dai-loan";
+            locationModel.OrientedCustomer = "Sinh viên, Gia đình, Giới...";
+
+            locationModel.AreaScore = 7.6;
+            locationModel.AddressScore = 7.3;
+            locationModel.ServiceScore = 7.0;
+            locationModel.CostScore = 6.3;
+            return View(locationModel);
         }
     }
 }
--- MvcApplication1/Models/LocationForDetailModel.cs
+++ MvcApplication1/Models/LocationForDetailModel.cs
@@ -8,10 +8,38 @@ namespace MvcApplication1.Models
     public class LocationForDetailModel
     {
         public int Id { get; set; }
+        /// <summary>
+        /// Ảnh đại diện của địa điểm
+        /// </summary>
         public string RepImgLink { get; set; }
+        /// <summary>
+        /// Tên thành phố
+        /// </summary>
         public string CityName { get; set; }
+        /// <summary>
+        /// Link thành phố
+        /// </summary>
+        public string CityLink { get; set; }
+        /// <summary>
+        /// Tên quận , huyện
+        /// </summary>
         public string DistrictName { get; set; }
+        /// <summary>
+        /// Link quận huyện
+        /// </summary>
+        public string DistrictLink { get; set; }
+        /// <summary>
+        /// Tên phường xã
+        /// </summary>
         public string WardName { get; set; }
+        /// <summary>
+        /// Link phường xã
+        /// </summary>
+        public string WardLink { get; set; }
+
+        /// <summary>
+        /// Tên địa điểm
+        /// </summary>
         public string LocationName { get; set; }
         /// <summary>
         /// Điểm chất lượng
@@ -33,7 +61,45 @@ namespace MvcApplication1.Models
         /// Điểm về giá cả
         /// </summary>
         public double CostScore { get; set; }
-
+        /// <summary>
+        /// Địa chỉ quán
+        /// </summary>
         public string Address { get; set; }
+        /// <summary>
+        /// Thời gian mở quán
+        /// </summary>
+        public DateTime? OpenTime { get; set; }
+        /// <summary>
+        /// Thời gian đóng quán
+        /// </summary>
+        public DateTime? ClosedTime { get; set; }
+        
+        public decimal? MinCost { get; set; }
+
+        public decimal? MaxCost { get; set; }
+
+        public string HrefLink { get; set; }
+        /// <summary>
+        /// Số lượt xem
+        /// </summary>
+        public int ViewCount { get; set; }
+        /// <summary>
+        /// Tên loại hình địa điểm
+        /// </summary>
+        public string CategoryName { get; set; }
+        /// <summary>
+        /// Link các địa điểm có loại hình liên quan
+        /// </summary>
+        public string CategoryLink { get; set; }
+        /// <summary>
+        /// Tên ẩm thực của địa điểm
+        /// </summary>
+        public string CulinaryName { get; set; }
+        /// <summary>
+        /// Link của loại ẩm thực đó
+        /// </summary>
+        public string CulinaryLink { get; set; }
+
+        public string OrientedCustomer { get; set; }
     }
 }
